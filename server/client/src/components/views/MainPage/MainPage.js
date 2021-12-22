14254949d9d27df374e22d0164d584d3c8784ba5
gonudayo <kgw010119@gmail.com>
2021-12-22 14:36:41 +0000

    feat: 만 나이 계산 기능

--- server/client/src/components/views/MainPage/MainPage.js
+++ server/client/src/components/views/MainPage/MainPage.js
@@ -5,6 +5,19 @@ import StockChart from './Sections/StockChart';
 import UsefulSite from './Sections/UsefulSite';
 import RoutineSite from './Sections/RoutineSite';
 
+// 만 나이 계산
+function seoul() {
+  const temp = new Date();
+  temp.setHours(temp.getHours() + 9);
+  return temp;
+}
+const today = seoul();
+const year = today.getFullYear();
+const month = `0${today.getMonth() + 1}`.slice(-2);
+const day = `0${today.getDate()}`.slice(-2);
+let age = year - 2001;
+if (month <= 1 && day <= 19) age -= 1;
+
 function MainPage() {
 	const [Grass, setGrass] = useState([]);
 	const [Commits, setCommits] = useState(0);
@@ -37,14 +50,14 @@ function MainPage() {
 		<div>
 			<div className="app" style={{ fontSize: '2rem' }}><img style={{ width: '300px', height: 'auto' }} src='/gunwoo.JPG' alt='김건우 초상화' ></img></div>
 			<br />
-			<div className="app" style={{ fontSize: '2rem' }}><p>"반갑습니다. 만 21세 초신성 개발자 <b>김건우</b> 입니다."</p></div>
+			<div className="app" style={{ fontSize: '2rem' }}><p>"반갑습니다. 만 {age}세 초신성 개발자 <b>김건우</b> 입니다."</p></div>
 			<br /><br /><br /><br />
 			<GrassChart data={Grass} commits={Commits} solves={Solves} />
 			<div className="app">
 			<br />
 			<StockChart data={Stock} asset={Asset} />
 			<br />
-			<div style={{ fontSize: '1.5rem' }}>루틴 및 유용한 사이트</div>
+			<div style={{ fontSize: '1.5rem' }}>루틴</div>
 			<br />
 			<UsefulSite />
 			<br />
